2ac54de8324e470ef046789adbf8d822018376f8
Sentiments <qianqian1903@gmail.com>
2024-03-06 23:12:07 +0800

    feat: added vite-svgr plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import { flatRoutes } from "remix-flat-routes";
 import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
     plugins: [
@@ -12,6 +13,7 @@ export default defineConfig({
             },
         }),
         tsconfigPaths(),
+        svgr(),
     ],
     server: {
         port: 8080,
